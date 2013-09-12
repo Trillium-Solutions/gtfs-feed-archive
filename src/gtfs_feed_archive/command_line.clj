65e297095356fab0ebc20f12de974906dbb18ad8
ed-g <edward.groth@gmail.com>
2013-09-11 23:41:52 -0700

    Split command-line into its own namespace.

---
+++ src/gtfs_feed_archive/command_line.clj
@@ -0,0 +1,43 @@
+(ns gtfs-feed-archive.command-line
+  (:refer-clojure :exclude [format])   ;; I like cl-format better...
+  (:require clojure.set)
+  (:use gtfs-feed-archive.util 
+        clojure.test
+        [clojure.tools.cli :only [cli]] ;; Command-line parsing.
+        [clojure.pprint :only [pprint]] 
+        [clojure.pprint :rename {cl-format format}]))
+
+(defn parse-args-or-die! [& args]
+  (let [set-merge (fn [previous key val]
+                    (assoc previous key
+                           (if-let [oldval (get previous key)]
+                             (merge oldval val)
+                             (hash-set val))))
+        parse-date (fn [arg] (when-let [d (clj-time.format/parse-local arg)] (.toDate d)))
+        cli-format ["Create a GTFS feed archive."
+                    ["-o" "--output-directory" "Directory to place output zip files into."]
+                    ["-i" "--input-csv"
+                     "Input CSV feed list file." :assoc-fn set-merge]
+                    ["-s" "--since-date"
+                     "Create an archive of feeds modified after date, e.g. 2013-08-23."
+                     :parse-fn parse-date
+                     :assoc-fn set-merge]
+                    ["-a" "--all"
+                     "Make an archive of all feeds." :default false :flag true]]
+        [_ _ usage-text] (apply cli nil cli-format)
+        print-usage-and-die! (fn [malfunction]
+                               (format t "~a~%~a~%" usage-text malfunction)
+                               (System/exit 1))
+        [options plain-args _]
+        (try (apply cli args cli-format)
+             (catch Exception e (do (println "Error parsing command line: " e)
+                                    (print-usage-and-die!))))]
+    ;; See if we have all the information we need to create an archive.
+    ;; If not, print usage information and bail.
+    (when-not (every? identity (:since options))
+      (print-usage-and-die! "Please format dates as RFC 3339: YYYY-MM-DD."))
+    (when-not (pos? (count (:input-csv options)))
+      (print-usage-and-die! "Please supply at least one input CSV feed list file."))
+    (when-not (pos? (count (:output-directory options)))
+      (print-usage-and-die! "Please indicate an output directory in which to create zip files."))
+    [options plain-args]))
--- src/gtfs_feed_archive/core.clj
+++ src/gtfs_feed_archive/core.clj
@@ -6,7 +6,8 @@
             [gtfs-feed-archive.javadoc-helper :as javadoc-helper]
             [gtfs-feed-archive.cache-persistance :as cache-persistance]
             [gtfs-feed-archive.cache-manager :as cache-manager]
-            [gtfs-feed-archive.download-agent :as download-agent])
+            [gtfs-feed-archive.download-agent :as download-agent]
+            [gtfs-feed-archive.command-line :as command-line])
   (:use gtfs-feed-archive.util 
         clojure.test
         clojure-csv.core
@@ -26,68 +27,19 @@
       (.putNextEntry z (java.util.zip.ZipEntry. name))
       (copy-data-to-stream content z))))
 
-(defn make-example-zip-file []
-  (make-zip-file "/tmp/foo.zip"
-                 [["foo/foo.txt" "foo\n"] ; test automatic string->bytes conversion.
-                  ["foo/bar.txt" (string->bytes "bar\n")]
-                  ["foo/baz.txt" (string->bytes "baz\n")]]))
+(defn read-csv-file [filename]
+  (with-open [r (clojure.java.io/reader filename)]
+      (doall (csv->maps r))))
 
 ;; convenience function. long term we want to manage the input CSV
 ;; file(s) using a user setting for which URL to grab the CSV file from.
 (defn public-gtfs-feeds [] 
-  (let [public-feeds "./resources/oregon_public_gtfs_feeds.csv"
-        public-feeds-august "./resources/oregon_public_gtfs_feeds.14-Aug-2013.csv"
-        public-feeds-working "./resources/oregon_public_gtfs_feeds.working.csv" ;; all working feeds.
-        public-feeds-smaller "./resources/oregon_public_gtfs_feeds.smaller.csv" ;; smaller feeds only.
-        ]
-    (with-open [r (clojure.java.io/reader public-feeds)]
-      (doall (csv->maps r)))))
-
-;;; for instance:
-;;; (error-feeds (feed-last-updates (example-csv-config))) 
-;;;  => ({:last-update nil, :feed-name "error"})
-(defn error-feeds [feed-updates]
-  (filter #(nil? (:last-update %))
-          feed-updates))
-
-(defn parse-args-or-die! [& args]
-  (let [set-merge (fn [previous key val]
-                    (assoc previous key
-                           (if-let [oldval (get previous key)]
-                             (merge oldval val)
-                             (hash-set val))))
-        parse-date #(when-let [d (clj-time.format/parse-local %)] (.toDate d))
-        cli-format ["Create a GTFS feed archive."
-                    ["-i" "--input-csv"
-                     "Input CSV feed list file." :assoc-fn set-merge]
-                    ["-s" "--since"
-                     "Archive of feeds modified since <DATE>, e.g. 2013-08-23."
-                     :parse-fn parse-date
-                     :assoc-fn set-merge]
-                    ["-a" "--all"
-                     "Create archive of all feeds." :default false :flag true]]
-        [_ _ usage-text] (apply cli nil cli-format)
-        print-usage-and-die! (fn [malfunction]
-                               (println usage-text)
-                               (println malfunction)
-                               (System/exit 1)) 
-        [flags remaining-args _]
-        (try (apply cli args cli-format)
-             (catch Exception e (do (println "Error parsing command line: " e)
-                                    (print-usage-and-die!))))]
-    (println "flags => " flags)
-    (println "remaining-args => " remaining-args)
-    ;; Now, see if we have all the information we need to create an archive.
-    ;; If not, print usage information and bail.
-    (when-not (every? identity (:since flags))
-      (print-usage-and-die! "Please format dates as RFC 3339: YYYY-MM-DD."))
-    (when-not (pos? (count (:input-csv flags)))
-      (print-usage-and-die! "Please supply at least one input CSV feed list file."))
-    [flags remaining-args]))
+  (let [public-feeds "./resources/oregon_public_gtfs_feeds.csv"]
+    (read-csv-file public-feeds)))
 
 (defn run-command-line [& args]
-  (println
-   (apply parse-args-or-die! args)))
+  (let [[options plain-args] (apply command-line/parse-args-or-die! args)]
+    ))
 
 (defn -main [& args]
   (apply run-command-line args))
