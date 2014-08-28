e2e8b82d35d114e9168cbced451e7ed8f0f56ed4
Ed Groth <edward.groth@gmail.com>
2014-08-28 22:41:41 +0000

    upgrade to latest library versions

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject gtfs-feed-archive "0.1.6"
+(defproject gtfs-feed-archive "0.1.8"
   :description "GTFS Feed Archive Tool"
   :url "http://github.com/ed-g/gtfs-feed-archive"
   :license {:name "GNU GPL version 3 or newer"
             :url "http://www.gnu.org"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.9"]  ;; HTTP client
-                 [com.velisco/clj-ftp "0.3.0"] ;; FTP client
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.0.0"]  ;; HTTP client
+                 [com.velisco/clj-ftp "0.3.1"] ;; FTP client
                  [clojure-csv/clojure-csv "2.0.1"] ;; CSV read/write
-                 [clj-time "0.6.0"] ;; Sane date and time library.
+                 [clj-time "0.8.0"] ;; Sane date and time library.
                  [org.clojure/tools.cli "0.3.1"] ;; Command-line parsing.
-                 [com.taoensso/timbre "2.7.1"] ;; logging
-                 [compojure "1.1.6"]
+                 [com.taoensso/timbre "3.2.1"] ;; logging
+                 [compojure "1.1.8"]
                  [hiccup "1.0.5"]
-                 [ring/ring-core "1.2.1"]
-                 [ring/ring-jetty-adapter "1.2.1"]
-                 [org.clojure/tools.nrepl "0.2.3"]]
+                 [ring/ring-core "1.3.1"]
+                 [ring/ring-jetty-adapter "1.3.1"]
+                 [org.clojure/tools.nrepl "0.2.4"]]
   :main gtfs-feed-archive.core
   :aot [gtfs-feed-archive.core])
